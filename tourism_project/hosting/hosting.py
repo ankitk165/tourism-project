ece8b80a9599fcb68249e1917d486a7a3aa859bf
ankitk165 <ankitk165@gmail.com>
2026-03-29 18:16:46 +0530

    added code to create space

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,7 +1,21 @@
 from huggingface_hub import HfApi
 import os
 
+repo_id = "ankit079/tourism-project"
+repo_type = "space"
+
+# Initialize API client
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="ankit079/tourism-project",          # the target repo
