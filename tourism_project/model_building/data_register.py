d5fadfa49060d4c20a69c1f963e12663d72695fd
ankitk165 <ankitk165@gmail.com>
2026-03-29 18:29:01 +0530

    added code to create space

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -3,7 +3,7 @@ from huggingface_hub import HfApi, create_repo
 import os
 
 
-repo_id = "ankit079/tourism-project"
+repo_id = "ankit079/tourism-pred-project"
 repo_type = "dataset"
 
 # Initialize API client
